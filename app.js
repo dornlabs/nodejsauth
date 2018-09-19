f8ca41b7343778c9d8f88308248ef213ba04ca67
Thomas <dornlabs@protonmail.com>
2018-09-18 22:44:50 -0600

    update

--- app.js
+++ app.js
@@ -10,26 +10,34 @@ app.use(bodyParser.urlencoded({extended: true}));
 let url =  mongoUriBuilder({  
             host: "localhost",
             port: 27017,
-            database: 'pearson',
-    });
-        
-class Auth {
-    
-    constructor( _url ){
-        this.url = _url
-    }
-    
-    async createUser(req, res, next ){
+            database: 'pearson'});
+
+async function userValidator( _username, _password ){
+        let query = {
+            username : _username,
+            password : _password
+        }
+        let conn =  await Mongo.connect( url , { useNewUrlParser: true } );
+        let db   =  await conn.db("pearson");
+        let doc  =  await db.collection("users").find(query).toArray()
+        if ( doc.length  == 1 ){
+            return true
+        } else {
+            return false
+        }
+}
+
+async function createUser(req, res, next ){
         let query = {
             username : req.body.username,
             password : req.body.password,
         }
         let create = async function(){
-            let conn =  await Mongo.connect( this.url , { useNewUrlParser: true } );
+            let conn =  await Mongo.connect( url , { useNewUrlParser: true } );
             let db   =  await conn.db("pearson");
             await db.collection("users").insertOne(query)
         }
-        if (await Auth.userValidator(query.username, query.password )==false){
+        if (await userValidator(query.username, query.password )==false){
             create()
             console.log("user created")
             req.usercreated = true
@@ -42,38 +50,21 @@ class Auth {
             next()
         }
     }
-    async verifyUser(req, res, next){   
+
+async function verifyUser(req, res, next){   
         let query = {
             username : req.body.username,
             password : req.body.password,
         }
         console.log(query)
-        if ( await Auth.userValidator(query.username, query.password )==true ){
+        if ( await userValidator(query.username, query.password )==true ){
             req.uservalid = true;
             next()
         }else{
             res.sendStatus(403);
         }
-    };
-    async userValidator( _username, _password ){
-        let query = {
-            username : _username,
-            password : _password
-        }
-        let conn =  await Mongo.connect(this.url , { useNewUrlParser: true } );
-        let db   =  await conn.db("pearson");
-        let doc  =  await db.collection("users").find(query).toArray()
-        if ( doc.length  == 1 ){
-            return true
-        } else {
-            return false
-        }
-    }
-  
 };
 
-
-let Auth_ = Auth(url);
 
 
 app.get('/', (req, res) => {
@@ -81,14 +72,14 @@ app.get('/', (req, res) => {
     message: '/login to login with post request. /create to create a new user'
   });
 });
-app.post('/login', Auth_.verifyUser , (req, res) => {
+app.post('/login', verifyUser , (req, res) => {
     if ( req.uservalid == true){
         res.json("valid")
     }else{
         res.json("try again");
     }
 });
-app.post('/create', Auth_.createUser, (req, res) => { 
+app.post('/create', createUser, (req, res) => { 
     console.log(req.usercreated);
     if ( req.usercreated == true){
         res.json("User created with: " + req.username + " " + req.password )
