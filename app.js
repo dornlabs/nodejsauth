17ba80805cbde61a80cfd358db52ebd6e90b3867
Thomas <dornlabs@protonmail.com>
2018-09-18 23:09:47 -0600

    update

--- app.js
+++ app.js
@@ -6,19 +6,18 @@ const Mongo = require('mongodb')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
+//returns the mongo url
 let url =  mongoUriBuilder({  
             host: "localhost",
             port: 27017,
             database: 'pearson'});
 
+//returns true if user is found false if not
 async function userValidator( _username, _password ){
-    
     let query = {
         username : _username,
         password : _password
     }
-    
     let conn =  await Mongo.connect( url , { useNewUrlParser: true } );
     let db   =  await conn.db("pearson");
     let doc  =  await db.collection("users").find(query).toArray()
@@ -28,24 +27,25 @@ async function userValidator( _username, _password ){
     } else {
         return false
     }
-
 }
-
+//put user in database
+async function putUser( _username, _password ){
+    let input = {
+        username: _username,
+        password: _password
+    }   
+    let conn =  await Mongo.connect( url , { useNewUrlParser: true } );
+    let db   =  await conn.db("pearson");
+    await db.collection("users").insertOne( input )
+}
+//midleware function that creates a user if non exists sents flag to created, if user already in db sents flag to false
 async function createUser(req, res, next ){
     let query = {
         username : req.body.username,
         password : req.body.password,
     }
-    
-    let create = async function(){
-    
-        let conn =  await Mongo.connect( url , { useNewUrlParser: true } );
-        let db   =  await conn.db("pearson");
-        await db.collection("users").insertOne(query)
-    }
-    
     if (await userValidator(query.username, query.password )==false){
-        create()
+        putUser( query.username, query.password )
         console.log("user created")
         req.usercreated = true
         req.username = query.username
@@ -57,7 +57,7 @@ async function createUser(req, res, next ){
         next()
     }
 }
-
+//middleware that validates the username
 async function verifyUser(req, res, next){   
     let query = {
         username : req.body.username,
@@ -71,8 +71,6 @@ async function verifyUser(req, res, next){
         res.sendStatus(403);
     }
 };
-
-
 
 app.get('/', (req, res) => {
   res.json({
@@ -89,7 +87,6 @@ app.post('/login', verifyUser , (req, res) => {
 });
 
 app.post('/create', createUser, (req, res) => { 
-    console.log(req.usercreated);
     if ( req.usercreated == true){
         res.json("User created with: " + req.username + " " + req.password )
     }else{
